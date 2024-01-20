e234aef24796fbc7924b89108773285a8cab4edb
WassCodeur <wachioubouraima56@gmail.com>
2024-01-20 01:07:33 +0000

    update

--- cats.py
+++ cats.py
@@ -70,13 +70,13 @@ async def on_change_cat(gevent: dict):
     """
     # Get the text that was shown in the current cat image. This was passed as
     # a parameter on the Action set for the button.
-    text =  "Hello"
-    # gevent.commonEventObject.parameters['text']
+    text =  str(gevent["commonEventObject"]["parameters"]["text"])
+    #gevent.commonEventObject.parameters['text']
 
     # The isHomepage parameter is passed as a string, so convert to a Boolean.
-    is_homepage = True
+    is_homepage = gevent["commonEventObject"] == True
     # gevent.commonEventObject.parameters['is_homepage'] == 'True'
-
+   
     # Create a new card with the same text.
     card = create_cat_card(text, is_homepage)
 
